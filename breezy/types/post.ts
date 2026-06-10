1de136e1508f6b4659f5ac6282df511eb53d88f0
Sunnickel12 <stevenvieira05@gmail.com>
2026-06-10 16:22:23 +0200

    Data types and structure

---
+++ breezy/types/post.ts
@@ -0,0 +1,13 @@
+import { User } from './user';
+
+export interface Post {
+  id: string;
+  author: User;
+  content: string;
+  createdAt: string;
+  likesCount: number;
+  commentsCount: number;
+  tags?: string[]; // Fx12: Optional tags
+  imageUrl?: string; // Fx13: Optional image URL
+  repostsCount: number; // Fx14: Reposts count
+}
