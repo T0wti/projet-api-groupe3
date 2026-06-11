17a3cea3595b47b33a330deff56709a9e315b713
T0wti <diego.borto31@gmail.com>
2026-06-12 00:21:05 +0200

    feat(i18n): implement i18next integration and add French translations for sidebar navigation

--- breezy/app/(main)/I18nProvider.tsx
+++ breezy/app/(main)/I18nProvider.tsx
@@ -1,8 +1,16 @@
-"use client"; // Ce fichier sera un composant client
+"use client";
 
+import './i18n';
 import React from 'react';
-import './i18n'; // On déplace l'import d'i18n ICI
+// 1. On importe explicitement l'instance configurée depuis ton fichier
+import i18n from './i18n'; 
+import { I18nextProvider } from 'react-i18next'; // 2. On importe le composant de contexte officiel
 
 export default function I18nProvider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    // 3. On entoure toute l'application avec l'instance i18n injectée de force
+    <I18nextProvider i18n={i18n}>
+      {children}
+    </I18nextProvider>
+  );
 }
